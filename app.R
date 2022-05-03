4788c38faa50eaf50c954ccd6d17065097cf7bbd
LayalChristine <layal.lettry@gmail.com>
2022-05-03 21:17:56 +0200

    styler and new entry

--- app.R
+++ app.R
@@ -10,48 +10,61 @@ library(shinythemes)
 ####################################
 # User Interface                   #
 ####################################
-ui <- fluidPage(theme = shinytheme("superhero"),
-                navbarPage("Stock-trading tools:",
-                           
-                           tabPanel("Home",
-                                    # Input values
-                                    sidebarPanel(
-                                      HTML("<h3>Input parameters</h3>"),
-                                      textInput(inputId = "ticker_symbol", 
-                                                label = "Ticker Symbol", 
-                                                value = "Enter a ticker symbol"),
-                                      textInput(inputId = "currency", 
-                                                label = "Currency", 
-                                                value = "Enter a currency"),
-                                      
-                                      
-                                      numericInput(inputId = "dividend_yield", 
-                                                   label = "Dividend Yield in percent", 
-                                                   value = 2, 
-                                                   min = 0, 
-                                                   max = 100, 
-                                                   step = 0.1),
-                                      
-                                      actionButton("submitbutton", 
-                                                   "Submit", 
-                                                   class = "btn btn-primary")
-                                    ),
-                                    
-                                    mainPanel(
-                                      tags$label(h3('Evaluation')), # Status/Output Text Box
-                                      verbatimTextOutput('contents'),
-                                      tableOutput('tabledata') # Results table
-                                    ) # mainPanel()
-                                    
-                           ), #tabPanel(), Home
-                           
-                           tabPanel("About", 
-                                    titlePanel("About"), 
-                                    div(includeMarkdown("about.md"), 
-                                        align="justify")
-                           ) #tabPanel(), About
-                           
-                ) # navbarPage()
+ui <- fluidPage(
+  theme = shinytheme("superhero"),
+  navbarPage(
+    "Stock-trading tools:",
+    tabPanel(
+      "Home",
+      # Input values
+      sidebarPanel(
+        HTML("<h3>Input parameters</h3>"),
+        textInput(
+          inputId = "ticker_symbol",
+          label = "Ticker Symbol",
+          value = ""
+        ),
+        textInput(
+          inputId = "currency",
+          label = "Currency",
+          value = ""
+        ),
+        numericInput(
+          inputId = "dividend_yield",
+          label = "Dividend Yield in percent",
+          value = 2,
+          min = 0,
+          max = 100,
+          step = 0.1
+        ),
+        numericInput(
+          inputId = "per",
+          label = "Price Earnings Ratio",
+          value = 10,
+          min = 0,
+          max = 100,
+          step = 0.01
+        ),
+        actionButton("submitbutton",
+          "Submit",
+          class = "btn btn-primary"
+        )
+      ),
+      mainPanel(
+        tags$label(h3("Evaluation")), # Status/Output Text Box
+        verbatimTextOutput("contents"),
+        tableOutput("tabledata") # Results table
+      ) # mainPanel()
+    ), # tabPanel(), Home
+
+    tabPanel(
+      "About",
+      titlePanel("About"),
+      div(includeMarkdown("about.md"),
+        align = "justify"
+      )
+    ) # tabPanel(), About
+  ) # navbarPage()
 ) # fluidPage()
 
 
@@ -59,35 +72,42 @@ ui <- fluidPage(theme = shinytheme("superhero"),
 # Server                           #
 ####################################
 server <- function(input, output, session) {
-  
+
+
   # Input Data
-  datasetInput <- reactive({  
-    
+  datasetInput <- reactive({
+
     # bmi <- input$weight/( (input$height/100) * (input$height/100) )
     # bmi <- data.frame(bmi)
     # names(bmi) <- "BMI"
     # print(bmi)
-    print(paste0(input$ticker_symbol, ": ",
-                 input$dividend_yield, " ", input$currency, " Dividend Yield"))
-    
+    # print(paste0(input$ticker_symbol, ": ",
+    #              input$dividend_yield, " ", input$currency, " Dividend Yield"))
+    final_eval <- if (
+      input$dividend_yield >= 2 &
+        input$per <= 20) {
+      paste0(input$ticker_symbol, " stock ", "is a good opportunity.")
+    } else {
+      paste0(input$ticker_symbol, " stock ", "is not a good opportunity.")
+    }
+    print(final_eval)
   })
-  
+
   # Status/Output Text Box
   output$contents <- renderPrint({
-    if (input$submitbutton>0) { 
-      isolate("Calculation complete.") 
+    if (input$submitbutton > 0) {
+      isolate("Calculation complete.")
     } else {
       return("Server is ready for calculation.")
     }
   })
-  
+
   # Prediction results table
   output$tabledata <- renderTable({
-    if (input$submitbutton>0) { 
-      isolate(datasetInput()) 
-    } 
+    if (input$submitbutton > 0) {
+      isolate(datasetInput())
+    }
   })
-  
 }
 
 
