162617491d62447c1898c12effcf2af40a4bd787
LayalChristine <layal.lettry@gmail.com>
2022-08-03 14:15:02 +0200

    Create formula to calculate final value without annuity and with simple interest

--- R/get_final_value.R
+++ R/get_final_value.R
@@ -2,9 +2,12 @@
 #' 
 #' @description Function computing the final of an investment project.
 #' 
+#' @param n_per Number of periods (years) over which the final value should be 
+#' calculated.
+#' 
 #' @param v_0 Initial investment capital.
 #' 
-#' @param exp_return Average expected return per year.
+#' @param exp_return Average expected return per year (in percent).
 #' 
 #' @param x_yearly Yearly annuity. Default = 0.
 #' 
@@ -15,10 +18,12 @@
 #' - `FINAL_VALUE`: Investment final value time serie.
 #' 
 #' @examples
-#' # Final value of immediate annuities with first capital and yearly expected of 10%.
+#' # Final value of immediate annuities with first capital and yearly expected of
+#' # 10% during 30 years.
 #' get_final_value( 
+#' n_per = 30,
 #' v_0 = 5000, 
-#' exp_return = 0.1, 
+#' exp_return = 10, 
 #' x_yearly = 12000, 
 #' immediate = TRUE
 #' )
@@ -28,11 +33,14 @@
 #' @export
 
 get_final_value <- function(
+    n_per,
     v_0,
     exp_return,
     x_yearly = 0,
     immediate = TRUE
 ){
   
-  FINAL_VALUE
+  FINAL_VALUE <- tibble(period = 1:n_per,
+                        final_value  = v_0 * (1 + period * (exp_return/100)))
+    
 }
